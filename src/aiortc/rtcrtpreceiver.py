c64e476f843c426e43849f8778c4b30215918d44
islandereleven <sean@vedrinelle.com>
2020-04-23 14:52:43 +0200

    packet poking

--- src/aiortc/rtcrtpreceiver.py
+++ src/aiortc/rtcrtpreceiver.py
@@ -425,7 +425,7 @@ class RTCRtpReceiver:
         Handle an incoming RTP packet.
         """
         self.__log_debug("< %s", packet)
-
+        self.__log_debug("< %s", packet.__dict__)
         # self.__log_debug(f"NACK {self.__nack_generator.missing}")
         # feed bitrate estimator
         if self.__remote_bitrate_estimator is not None:
